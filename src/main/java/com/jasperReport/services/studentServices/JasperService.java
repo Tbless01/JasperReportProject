e7d6a85449f4baff891cf59d02c0fad5e7c3f487
Blessing Olaitan <114719772+Tbless01@users.noreply.github.com>
2025-01-31 22:22:08 +0100

    report updated

--- src/main/java/com/jasperReport/services/studentServices/JasperService.java
+++ src/main/java/com/jasperReport/services/studentServices/JasperService.java
@@ -7,15 +7,23 @@ import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class JasperService {
 
-    public void getReportContext(String fileName, StudentResult studentResult) throws JRException {
+//    public void getReportContext(String fileName, List<StudentResult> studentResult) throws JRException {
+public void getReportContext(List<StudentResult> studentResult) throws JRException {
 
         JRBeanCollectionDataSource jrBeanCollectionDataSource = new JRBeanCollectionDataSource(studentResult);
-        JasperReport report = JasperCompileManager.compileReport(fileName);
+        JasperReport report = JasperCompileManager.compileReport(filePath);
+        Map<String, Object> parameters = new HashMap<>();
+        // You can add any parameters you need here, for example:
+         parameters.put("someKey", studentResult.);
         JasperPrint print = JasperFillManager.fillReport(report, jrBeanCollectionDataSource, n);
         JasperExportManager.exportReportToPdfFile(print, "");
         log.info("Report exported");
